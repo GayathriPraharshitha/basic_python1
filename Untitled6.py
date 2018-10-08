3bc662c8614cadfd6fddbd0aaaa195f1cc37d4d9
Gayathri Praharshitha <reddy.gayathripraharshitha@gmail.com>
2018-10-08 21:12:27 +0530

    Initial commit

---
+++ Untitled6.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+df=pd.DataFrame({'house number':[40,80,70,50,30], 'rent':[99,54,76,87,66]})
+plt.plot(df)
+
+
+
+
